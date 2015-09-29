301bdea1409a1d2fa8f1e9475c5273a33e9a78c4
NoFuture <brian.starrett@gmail.com>
2015-09-29 17:44:42 -0500

    added some notes to timeline

--- Timeline/trunk/Code/NoFuture/Timeline/Occidental/17_CE1788_1865.cs
+++ Timeline/trunk/Code/NoFuture/Timeline/Occidental/17_CE1788_1865.cs
@@ -46,11 +46,37 @@ namespace NoFuture.Timeline
             engd.AddEntry(1858, "[E. Smith-Stanley -1852\\1858-59]", PrintLocation.Left);
             engd.AddEntry(1860, "[H. John Temple 1855-58\\59-65]", PrintLocation.Left);
 
+
+            var fr = new Block {Ruler = rule, Title = "France"};
+            fr.AddEntry(1789, "Estates-General meets(1789)**");
+            fr.AddEntry(1791, "Storming of the Bastille(1789)");
+            fr.AddEntry(1792, "Louis XVI executed(1793)");
+            fr.AddEntry(1793, "Reign of Terror(1793\\94)");
+            fr.AddEntry(1794, "Robespierre executed(1794)");
+            fr.AddEntry(1795, "Napolean quells insurrection(1795)");
+            var de = new Block {Ruler = rule, Title = "Prussia"};
+
+            var aus = new Block { Ruler = rule, Title = "Austrian Empire" };
+
+            var ott = new Block { Ruler = rule, Title = "Ottoman Empire" };
+
+            var rus = new Block { Ruler = rule, Title = "Imperial Russia"};
+
             var plate = new Plate { Ruler = rule, Name = "Modern Age(1)" };
             plate.AddBlock(usa);
             plate.AddBlock(engd);
+            plate.AddBlock(fr);
+            plate.AddBlock(de);
+            plate.AddBlock(aus);
+            plate.AddBlock(ott);
+            plate.AddBlock(rus);
 
-            plate.Notes.Add("(*) last Prime Minister dismissed by a king");
+            plate.AddArrow(new Arrow(aus, ott){Text = "B.of Focsani(1789)"});
+            plate.AddArrow(new Arrow(rus, ott));
+
+            plate.Notes.Add("(*)   last Prime Minister dismissed by a king");
+            plate.Notes.Add("(**)  last met in 1614");
+
 
             return plate;
         }
