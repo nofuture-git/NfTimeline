fdf782144d41120d558e6c6ad3c30b810b84af9e
NoFuture <brian.starrett@gmail.com>
2016-11-29 11:23:01 -0600

    fixed defect in Mssql/Etc.cs for MSSQL server names with are qualified with a port

--- Timeline/trunk/Code/NoFuture/Timeline/Occidental/19_CE1914_1945.cs
+++ Timeline/trunk/Code/NoFuture/Timeline/Occidental/19_CE1914_1945.cs
@@ -11,9 +11,20 @@ namespace NoFuture.Timeline
         public Plate CE1914to1945()
         {
             var rule = new Rule { StartValue = 1914, EndValue = 1946, RuleLineSpacing = 7 };
-            var usa = new Block { Ruler = rule, Title = "United States" };
+            var usa = new Block { Ruler = rule, Title = "United States", Width = 50};
             usa.AddEntry(1917, "enters WWI", PrintLocation.Center);
-
+            usa.AddEntry(new LeaderEntry("Woodrow Wilson", new int?[,] { { 1913, 1921 } }) { StartValue = 1914.6 });
+            usa.AddEntry(new LeaderEntry("Warren G.Harding", new int?[,] { { 1921, 1923 } }) { StartValue = 1921 });
+            usa.AddEntry(new LeaderEntry("Calvin Coolidge", new int?[,] { { 1923, 1929 } }) { StartValue = 1923 });
+            usa.AddEntry(new LeaderEntry("Herbert Hoover", new int?[,] { { 1929, 1933 } }) { StartValue = 1929 });
+            usa.AddEntry(new LeaderEntry("F.D. Roosevelt", new int?[,] { { 1933, 1945 } }) { StartValue = 1933 });
+            usa.AddEntry(1920,"Women gain voting rights(1920)");
+            usa.AddEntry(1920.6,1933,"Prohibition(1920/33)", PrintLocation.Left);
+            usa.AddEntry(1924,"Immigration Act(1924)[4]");
+            usa.AddEntry(1929.6, "Stock Market Crash(1929)", PrintLocation.Right);
+            usa.AddEntry(1930,1935,"Great Depression", PrintLocation.Right);
+            usa.AddEntry(1933.3,1935,"Dust Bowl(1933/35)", PrintLocation.Left);
+            usa.AddEntry(1935, "Social Security Act(1935)");
             var engd = new Block { Ruler = rule, Title = "United Kingdom"};
 
             var fr = new Block { Ruler = rule, Title = "France"};
@@ -29,7 +40,7 @@ namespace NoFuture.Timeline
             rus.AddEntry(1918,"Tsar & family executed(1918)");
             rus.AddEntry(1918.6, "Red Terror");
             rus.AddEntry(1920,"Whites retreat east(1920)");
-            rus.AddEntry(1922,"total devastation **");
+            rus.AddEntry(1922,"total devastation[2]");
 
             var ussr = new Block {Ruler = rule, Title = "Soviet Union", StartValue = 1917};
             ussr.AddEntry(1917.6,"Bolshevik Revolution(1917)");
@@ -37,8 +48,10 @@ namespace NoFuture.Timeline
             ussr.AddEntry(1919.6, "Polish-Soviet War(1919\\20)");
             ussr.AddEntry(new LeaderEntry("Lenin", new int?[,] { {null, 1924} }) {StartValue = 1922});
             ussr.AddEntry(new LeaderEntry("Stalin", new int?[,] { { 1924, 1953 } }) { StartValue = 1924 });
-            ussr.AddEntry(1928,1932,"First Five Year Plan");
-            ussr.AddEntry(1933,1937,"Second Five Year Plan");
+            ussr.AddEntry(1928,1932,"First Five Year Plan", PrintLocation.Right);
+            ussr.AddEntry(1932,1933,"Soviet famine", PrintLocation.Left);
+            ussr.AddEntry(1933,1937,"Second Five Year Plan", PrintLocation.Right);
+            ussr.AddEntry(1936,1938,"Great Purge[3]", PrintLocation.Left);
 
             var zho = new Block {Ruler = rule, Title = "Republic of China"};
 
@@ -58,7 +71,7 @@ namespace NoFuture.Timeline
             plate.AddBlock(jpn);
 
             plate.AddArrow(new Arrow(de, rus) { StartValue = 1914.6, Text = "Battle of Tannenberg(1914)" });
-            plate.AddArrow(new Arrow(de,fr) {StartValue = 1915, Text = "2nd Battle of Ypres(1915)*"});
+            plate.AddArrow(new Arrow(de,fr) {StartValue = 1915, Text = "2nd Battle of Ypres(1915)[1]"});
             
             plate.AddArrow(new Arrow(fr, de) { StartValue = 1916, Text = "Battle of Verdun(1916)" });
             plate.AddArrow(new Arrow(fr, de) { StartValue = 1916.6, Text = "Battle of Somme(1916)" });
@@ -72,8 +85,10 @@ namespace NoFuture.Timeline
             //The Battle of Verdun
             //The Battle of Passchendaele
             //Battle of the Somme
-            plate.Notes.Add(" * First lethal use of chemical weapons.");
-            plate.Notes.Add(" ** war and famine, millions dead, industry ruined, skilled and edu workers fled");
+            plate.Notes.Add(" [1] First lethal use of chemical weapons.");
+            plate.Notes.Add(" [2] war and famine, millions dead, industry ruined, skilled and edu workers fled");
+            plate.Notes.Add(" [3] 600K-1.2M deaths estimated.");
+            plate.Notes.Add(" [4] East & South European: limited, African: severely limited, Asian and Arab: banned, Latin American: no restriction");
             return plate;
         }
     }
