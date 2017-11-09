816fbbd531fecf30e990598c6e8ca412ab1683ac
NoFuture <brian.starrett@gmail.com>
2017-11-09 08:42:57 -0600

    refactored Nf Shared to also have a Core asm, changed references as needed

--- Timeline/trunk/Code/NoFuture/Timeline/Drawings.cs
+++ Timeline/trunk/Code/NoFuture/Timeline/Drawings.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using NoFuture.Exceptions;
 using NoFuture.Shared;
+using NoFuture.Shared.Core;
 
 namespace NoFuture.Timeline
 {
