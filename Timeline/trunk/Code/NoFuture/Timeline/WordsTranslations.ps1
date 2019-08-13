6dd717886affe64892eb9528e1c8f4ef2a9f5ca9
NoFuture <brian.starrett@gmail.com>
2019-08-13 09:51:25 -0500

    Nf.Timeline, added in simple cmdlet to look up english words across the three lists

---
+++ Timeline/trunk/Code/NoFuture/Timeline/WordsTranslations.ps1
@@ -0,0 +1,84 @@
+﻿$myScriptLocation = Split-Path $PSCommandPath -Parent
+
+$WordTranslations = @{
+    Hebrew = @{
+        FileName = "HebrewWords.txt";
+        Content = @()
+    }
+
+    Greek = @{
+        FileName = "GreekWords.txt";
+        Content = @()
+    }
+
+    Latin = @{
+        FileName = "LatinWords.txt";
+        Content = @()
+    }
+}
+
+:nextLang foreach($lang in $WordTranslations.Keys){
+     
+     $fn = $WordTranslations[$lang].FileName
+     if([string]::IsNullOrWhiteSpace($fn)){
+        continue nextLang;
+     }
+
+     $ffn = Join-Path $myScriptLocation $fn
+     if(-not (Test-Path $ffn)){
+        continue nextLang;
+     }
+
+     $WordTranslations[$lang].Content = Import-Csv -Path $ffn
+
+}
+
+<#
+    .SYNOPSIS
+    Returns Hebrew, Greek and Latin words by English translation
+    
+    .DESCRIPTION
+    Returns Hebrew, Greek and Latin words by English translation.
+    Based on the words learned from the Memrise app whose translations
+    are supposed to be committed to memory by now...
+
+    .PARAMETER EnglishWord
+    The English word whose translation is sought
+#>
+function Get-WordTranslations
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true,position=0)]
+        [string] $EnglishWord
+    )
+    Process
+    {
+        $results = @()
+
+        if($WordTranslations -eq $null){
+            throw "No CSV data has been loaded."
+            break;
+        }
+
+        :nextLang foreach($lang in $WordTranslations.Keys){
+            if([string]::IsNullOrWhiteSpace($lang)){
+                continue nextLang;
+            }
+
+            :nextLine foreach($line in $WordTranslations[$lang].Content){
+                if([string]::IsNullOrWhiteSpace($line.Translations)){
+                    continue nextLine;
+                }
+
+                $lowerTranslations = $line.Translations.ToLower();
+                if($lowerTranslations.Contains($EnglishWord.ToLower())){
+                    $results += $line
+                }
+            }
+        }
+
+        return $results
+    }
+}
